b92207773d05cf5f6b4a973353a7fc0105d3d978
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2024-11-04 13:58:31 -0500

    dfzgh

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,1 +1,32 @@
-import '../css/style.css'
+import '../CSS/style.css'
+
+
+Worlds = [
+    {"id": 101, "rarity": "common", "price": 0.4596, "rank": 7855},
+    {"id": 102, "rarity": "uncommon", "price": 0.8923, "rank": 4321},
+    {"id": 103, "rarity": "rare", "price": 1.2375, "rank": 1254},
+    {"id": 104, "rarity": "epic", "price": 2.5000, "rank": 678},
+    {"id": 105, "rarity": "legendary", "price": 5.7500, "rank": 111},
+    {"id": 106, "rarity": "mythic", "price": 12.0000, "rank": 25},
+    {"id": 107, "rarity": "common", "price": 0.3500, "rank": 8000},
+    {"id": 108, "rarity": "uncommon", "price": 0.6500, "rank": 4500},
+    {"id": 109, "rarity": "rare", "price": 1.9000, "rank": 1340},
+    {"id": 110, "rarity": "epic", "price": 3.3000, "rank": 567}
+]
+
+function addCard(id, image, rarity, rank, price) {
+    DOMSelectors.container.insertAdjacentHTML(
+        "beforeend",
+        `<div class="world">
+            <h2>World ${id}</h2>
+            <div class="imageholder">
+                <img src="${image}" alt="World Image" class="image">
+            </div>
+            <h3>${rarity}</h3>
+            <h4>${rank}/10000</h4>
+            <h4>${price}/10000</h4>
+        </div>`
+    )
+};
+
+Worlds.forEach(item => addCard(item.id, none, item.rarity, item.rank, item.price))
