c954b6adf6cd5a721ae0983fb3e546dfcf7bf5c0
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2024-11-14 00:47:37 -0500

    asdad

--- app/JS/main.js
+++ app/JS/main.js
@@ -2,16 +2,16 @@ import '../CSS/style.css'
 import hWorlds from "../JS/worlds.js"
 
 const Worlds = [
-    {"id": 101, "image": "../public/HytopiaWorld1.png", "rarity": "Common", "price": 0.4596, "rank": 7855},
-    {"id": 102, "image": "../public/HytopiaWorld2.png", "rarity": "Common", "price": 0.3500, "rank": 8000},
-    {"id": 103, "image": "../public/HytopiaWorld3.png", "rarity": "Rare", "price": 1.2375, "rank": 1254},
-    {"id": 104, "image": "../public/HytopiaWorld4.png", "rarity": "Epic", "price": 2.5000, "rank": 678},
-    {"id": 105, "image": "../public/HytopiaWorld5.png", "rarity": "Legendary", "price": 5.7500, "rank": 111},
-    {"id": 106, "image": "../public/HytopiaWorld6.png", "rarity": "Mythic", "price": 12.0000, "rank": 25},
-    {"id": 107, "image": "../public/HytopiaWorld7.png", "rarity": "Uncommon", "price": 0.8923, "rank": 4321},
-    {"id": 108, "image": "../public/HytopiaWorld8.png", "rarity": "Uncommon", "price": 0.6500, "rank": 4500},
-    {"id": 109, "image": "../public/HytopiaWorld9.png", "rarity": "Rare", "price": 1.9000, "rank": 1340},
-    {"id": 110, "image": "../public/HytopiaWorld10.png", "rarity": "Epic", "price": 3.3000, "rank": 567}
+    {"id": 101, "image": "public/HytopiaWorld1.png", "rarity": "Common", "price": 0.4596, "rank": 7855},
+    {"id": 102, "image": "public/HytopiaWorld2.png", "rarity": "Common", "price": 0.3500, "rank": 8000},
+    {"id": 103, "image": "public/HytopiaWorld3.png", "rarity": "Rare", "price": 1.2375, "rank": 1254},
+    {"id": 104, "image": "public/HytopiaWorld4.png", "rarity": "Epic", "price": 2.5000, "rank": 678},
+    {"id": 105, "image": "public/HytopiaWorld5.png", "rarity": "Legendary", "price": 5.7500, "rank": 111},
+    {"id": 106, "image": "public/HytopiaWorld6.png", "rarity": "Mythic", "price": 12.0000, "rank": 25},
+    {"id": 107, "image": "public/HytopiaWorld7.png", "rarity": "Uncommon", "price": 0.8923, "rank": 4321},
+    {"id": 108, "image": "public/HytopiaWorld8.png", "rarity": "Uncommon", "price": 0.6500, "rank": 4500},
+    {"id": 109, "image": "public/HytopiaWorld9.png", "rarity": "Rare", "price": 1.9000, "rank": 1340},
+    {"id": 110, "image": "public/HytopiaWorld10.png", "rarity": "Epic", "price": 3.3000, "rank": 567}
 ]
 
 const DOMSelectors = {
